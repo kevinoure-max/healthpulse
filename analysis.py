d7efe6baacf326d6909d5ce6b28c43a1c5f09366
Kevin <kevinoure@yahoo.fr>
2026-05-31 19:08:43 +0200

    feat: implement compute_stats function for statistical analysis of DataFrame

---
+++ analysis.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+def compute_stats(df: pd.DataFrame) -> dict:
+    latest_year = df["Year"].max()
+    latest_value = df[df["Year"] == latest_year]["value"].values[0]
+    min_value = df["value"].min()
+    max_value = df["value"].max()
+    min_year = df["Year"].min()
+    max_year = latest_year
+
+    earliest_value = df[df["Year"] == min_year]["value"].values[0]
+    change_absolute = latest_value - earliest_value
+    change_pct = ((latest_value - earliest_value) / earliest_value) * 100
+
+    mean = df["value"].mean()
+    data_points = len(df["value"])
+
+    return {
+        "latest_year": latest_year,
+        "latest_value": round(latest_value, 2),
+        "min_value": round(min_value, 2),
+        "max_value": round(max_value, 2),
+        "min_year": min_year,
+        "max_year": max_year,
+        "change_absolute": round(change_absolute, 2),
+        "change_pct": round(change_pct, 2),
+        "mean": round(mean, 2),
+        "data_points": data_points,
+    }
